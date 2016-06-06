40c48e70a69ed06dab3331fd0a1204b89f26dee1
nirbhayph <pherwani37@gmail.com>
2016-06-06 11:41:31 +0530

    Update settings.js

--- home-page-for-web-interface/js/settings.js
+++ home-page-for-web-interface/js/settings.js
@@ -1,12 +1,4 @@
-//  ====================================================================
-//	Theme Name: Spando - Multi-purpose Bootstrap Template
-//	Theme URI: http://themeforest.net/user/responsiveexperts
-//	Description: This javascript file is using as a settings file. This file includes the sub scripts for the javascripts used in this template.
-//	Version: 1.0
-//	Author: Responsive Experts
-//	Author URI: http://themeforest.net/user/responsiveexperts
-//	Tags:
-//  ====================================================================
+
 
 //	TABLE OF CONTENTS
 //	---------------------------
